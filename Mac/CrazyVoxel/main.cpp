24b1987ff9ab6be1b17c20a40951950f6e298452
Frahaan Hussain <Sonar Systems>
2015-11-07 15:44:51 +0000

    2nd Commit

--- Mac/CrazyVoxel/main.cpp
+++ Mac/CrazyVoxel/main.cpp
@@ -1,60 +1,145 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <vector>
 
-int main(void)
+using namespace std;
+
+int main( void )
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
     
     /* Initialize the library */
-    if (!glfwInit())
+    if ( !glfwInit( ) )
     {
         return -1;
     }
     
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1280, 1280, "Hello World", NULL, NULL);
-    if (!window)
+    window = glfwCreateWindow( 1280, 1280, "Hello World", NULL, NULL );
+    
+    if ( !window )
     {
-        glfwTerminate();
+        glfwTerminate( );
         return -1;
     }
     
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent( window );
     
-    glEnable(GL_DEPTH_CLAMP);
-    glMatrixMode(GL_PROJECTION);
+    glEnable( GL_DEPTH_CLAMP );
+    glMatrixMode( GL_PROJECTION );
+    //glCullFace( GL_FRONT_AND_BACK );
+    glEnable( GL_DEPTH_TEST );
     
     float angle = 0.0f;
     
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while ( !glfwWindowShouldClose( window ) )
     {
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-        glLoadIdentity();
-        glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        glLoadIdentity( );
+        //glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
         
         /* Render OpenGL here */
-        
-
-        glPushMatrix();
+        /*glPushMatrix();
         glRotatef(angle, 1, 1, 1);
         glBegin( GL_POLYGON );
         // front
         glColor3f(1.0, 0.0, 0.0);
-        glVertex3f( -0.5, 0.5, 0.0 );
-        glVertex3f( 0.5, 0.5, 0.0 );
-        glVertex3f( 0.5, -0.5, 0.0 );
-        glVertex3f( -0.5, -0.5, 0.0 );
+        glVertex3f( -0.5, 0.5, 0.5 );
+        glVertex3f( 0.5, 0.5, 0.5 );
+        glVertex3f( 0.5, -0.5, 0.5 );
+        glVertex3f( -0.5, -0.5, 0.5 );
+        glEnd();
         
+        glBegin( GL_POLYGON );
         //top
         glColor3f(0.0, 1.0, 0.0);
-        glVertex3f( -0.5, 0.5, 0.0 );
-        glVertex3f( 0.5, 0.5, 0.0 );
-        glVertex3f( 0.5, 0.5, -1 );
-        glVertex3f( -0.5, 0.5, -1 );
+        glVertex3f( -0.5, 0.5, 0.5 );
+        glVertex3f( 0.5, 0.5, 0.5 );
+        glVertex3f( 0.5, 0.5, -0.5 );
+        glVertex3f( -0.5, 0.5, -0.5 );
         glEnd( );
-        glPopMatrix();
+        
+        glBegin( GL_POLYGON );
+        //bottom
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex3f( -0.5, -0.5, 0.5 );
+        glVertex3f( 0.5, -0.5, 0.5 );
+        glVertex3f( 0.5, -0.5, -0.5 );
+        glVertex3f( -0.5, -0.5, -0.5 );
+        glEnd( );
+        
+        glBegin( GL_POLYGON );
+        // back
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex3f( -0.5, 0.5, -0.5 );
+        glVertex3f( 0.5, 0.5, -0.5 );
+        glVertex3f( 0.5, -0.5, -0.5 );
+        glVertex3f( -0.5, -0.5, -0.5 );
+        glEnd( );
+        
+        glBegin( GL_POLYGON );
+        //right
+        glColor3f(0.3, 0.4, 1.0);
+        glVertex3f( 0.5, 0.5, 0.5 );
+        glVertex3f( 0.5, -0.5, 0.5 );
+        glVertex3f( 0.5, -0.5, -0.5 );
+        glVertex3f( 0.5, 0.5, -0.5 );
+        glEnd( );
+        
+        glBegin( GL_POLYGON );
+        //left
+        glColor3f(0.9, 0.6, 0.76);
+        glVertex3f( -0.5, 0.5, 0.5 );
+        glVertex3f( -0.5, -0.5, 0.5 );
+        glVertex3f( -0.5, -0.5, -0.5 );
+        glVertex3f( -0.5, 0.5, -0.5 );
+        glEnd( );
+        glPopMatrix();*/
+        
+        /*
+         // triangle
+        float f[] = {0, 0.5, -0.5, -0.5, -0.5, -0.5, 0.5, -0.5, -0.5};
+        
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, f );
+        glDrawArrays( GL_TRIANGLES, 0, 3 );
+        glDisableClientState( GL_VERTEX_ARRAY );
+         */
+        
+        /*
+        // square
+        float f[] = {-0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, -0.5, 0.5, -0.5, -0.5, 0.5};
+        
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, f );
+        glDrawArrays( GL_QUADS, 0, 4 );
+        glDisableClientState( GL_VERTEX_ARRAY );
+        */
+        
+        // cube
+        float f[] =
+        {
+            -0.5, 0.5, 0.5,
+            0.5, 0.5, 0.5,
+            0.5, -0.5, 0.5,
+            -0.5, -0.5, 0.5,
+            -0.5, 0.5, -0.5,
+            0.5, 0.5, -0.5,
+            0.5, -0.5, -0.5,
+            -0.5, -0.5, -0.5
+        };
+        
+        glPushMatrix();
+        glRotatef(angle, 1, 1, 1);
+        glEnableClientState( GL_VERTEX_ARRAY );
+        glVertexPointer( 3, GL_FLOAT, 0, f );
+        glDrawArrays( GL_QUADS, 0, 8 );
+        glDisableClientState( GL_VERTEX_ARRAY );
+        glEnd( );
+        
+        
 /*
         
         glPushMatrix();
